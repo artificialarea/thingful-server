37b98fb8421c912a00d6697ef26c009f8ac79c44
Sacha Sedriks <sachased@gmail.com>
2020-08-15 15:15:47 -0700

    temp for git diff troubleshooting

--- src/reviews/reviews-router.js
+++ src/reviews/reviews-router.js
@@ -4,43 +4,43 @@
 // SULTANA VERSION BELOW DOES
 // it's a spelling error somewhere...
 
-// const express = require('express')
-// const path = require('path')
-// const ReviewsService = require('./reviews-service')
-// const { requireAuth } = require('../middleware/basic-auth')
+const express = require('express')
+const path = require('path')
+const ReviewsService = require('./reviews-service')
+const { requireAuth } = require('../middleware/basic-auth')
 
-// const reviewsRouter = express.Router()
-// const jsonBodyParser = express.json()
+const reviewsRouter = express.Router()
+const jsonBodyParser = express.json()
 
-// reviewsRouter
-//     .route('/')
-//     .post(requireAuth, jsonBodyParser, (req, res, next) => {
-//         const { thing_id, rating, text } = req.body
-//         const newReview = { thing_id, rating, text }
+reviewsRouter
+    .route('/')
+    .post(requireAuth, jsonBodyParser, (req, res, next) => {
+        const { thing_id, rating, text } = req.body
+        const newReview = { thing_id, rating, text }
 
-//         for (const [key, value] of Object.entries(newReview))
-//             if (value == null)
-//                 return res.status(400).json({
-//                     error: `Missing '${key}' in request body`
-//                 })
+        for (const [key, value] of Object.entries(newReview))
+            if (value == null)
+                return res.status(400).json({
+                    error: `Missing '${key}' in request body`
+                })
         
-//         // console.log(req.user_id);
-//         newReview.user_id = req.user_id
+        // console.log(req.user_id);
+        newReview.user_id = req.user_id
 
-//         ReviewsService.insertReview(
-//             req.app.get('db'),
-//             newReview
-//         )
-//             .then(review => {
-//                 res
-//                     .status(201)
-//                     .location(path.posix.join(req.originalUrl, `/${review.id}`))
-//                     .json(ReviewsService.serializeReview(review))
-//             })
-//             .catch(next)
-//     })
+        ReviewsService.insertReview(
+            req.app.get('db'),
+            newReview
+        )
+            .then(review => {
+                res
+                    .status(201)
+                    .location(path.posix.join(req.originalUrl, `/${review.id}`))
+                    .json(ReviewsService.serializeReview(review))
+            })
+            .catch(next)
+    })
 
-// module.exports = reviewsRouter
+module.exports = reviewsRouter
 
 
 // SULTANA VERSION WORKS /////////////////////////////////////////
